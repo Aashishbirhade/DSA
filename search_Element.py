564bd993fefa2a598a9cbf95ede05cbfa1258f11
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2024-05-02 18:40:44 +0530

    search_Element.py

--- search_Element.py
+++ search_Element.py
@@ -1,8 +1,10 @@
-
 n,t = map(int,input().split())
 l = list(map(int,input().split()))
 if  t in l:
     print("yes")
 else:
     print("no")
-
+for i in l:
+    if i%2 == 0:
+        print("even no :- ",i)
+        
