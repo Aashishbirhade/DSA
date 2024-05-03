7ffcd379a07cf168496c048951cdecb836026a0b
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2024-05-03 14:39:52 +0530

    min_Max.py

--- min_Max.py
+++ min_Max.py
@@ -1,9 +1,9 @@
 li = list(map(int,input().split()))
 min = max = li[0]
 for i in li:
-    if max >=i:
-        min = i
-    else:
+    if max > i:
         max = i
+    elif min < i:
+        min = i
 print("Max = ",max)
 print("min = ", min)
