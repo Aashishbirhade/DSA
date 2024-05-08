7134ff201616b53d3e07b5eb94d04d4ba01e46c3
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2024-05-08 13:24:37 +0530

    pair_Sum.py

--- pair_Sum.py
+++ pair_Sum.py
@@ -2,7 +2,7 @@ a = list(map(int,input().split()))
 m = int(input())
 c = 0
 for i in range(len(a)):
-  for j in range(i,len(a)):
+  for j in range(i+1,len(a)):
     if a[i] + a[j] == m:
       c=c+1
 print(c)
